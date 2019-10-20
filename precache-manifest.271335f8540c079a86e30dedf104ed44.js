affe6943c78f63860c0d236525ee3220caf19890
Kevin Feng <fengfengkevin@gmail.com>
2019-10-20 17:02:29 -0400

    Updates

---
+++ precache-manifest.271335f8540c079a86e30dedf104ed44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dd4c9b1da99ee293f33371b7e16390",
+    "url": "/brain-frontend/index.html"
+  },
+  {
+    "revision": "cae29839001582c85a21",
+    "url": "/brain-frontend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "013fc28c802676c219ea",
+    "url": "/brain-frontend/static/css/main.f97fe1d9.chunk.css"
+  },
+  {
+    "revision": "cae29839001582c85a21",
+    "url": "/brain-frontend/static/js/2.cd7598e0.chunk.js"
+  },
+  {
+    "revision": "013fc28c802676c219ea",
+    "url": "/brain-frontend/static/js/main.5458c3f0.chunk.js"
+  },
+  {
+    "revision": "eb7587212855a39a7a25",
+    "url": "/brain-frontend/static/js/runtime-main.2b17edbb.js"
+  }
+]);
